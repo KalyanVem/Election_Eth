81f25becc02ca31b9f75b269d611203eb92cdf3e
KalyanVem <vkalyan08499@gmail.com>
2020-07-28 19:26:58 +0530

    cast votes

--- test/election.js
+++ test/election.js
@@ -20,5 +20,16 @@ contract('Election', (accounts) => {
         assert.equal(candidate2[0], 2, "Contains correct id");
         assert.equal(candidate2[1], "Candidate 2", "Contains correct name");
         assert.equal(candidate2[2], 0, "Contains correct vote count");
+    })
+    
+    it('allow a voter to cast vote', async () => {
+        candidateId = 1;
+        const candCount = await this.election.vote(candidateId, {from : "0xf683E000ec3dc60876a2BE81a08aeD8F141816b2"});
+        
+        const candidate1 = await this.election.candidates(1);
+        var voteCount = candidate1[2];
+        voted = await this.election.voters("0xf683E000ec3dc60876a2BE81a08aeD8F141816b2");
+        assert(voted, "the voter was marked as voted");
+        assert.equal(voteCount, 1, "incremented the candidates vote count");
     });
 })
